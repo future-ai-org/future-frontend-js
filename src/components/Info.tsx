0a5f13e48397c5d4ef9c82dd049c0a3e3fec9e08
von-steinkirch <b@bt3gl.xyz>
2025-04-24 13:43:19 -0700

    start adding new menus

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -4,10 +4,10 @@ import strings from "../i18n/info.json";
 export default function Info() {
   return (
     <div className="infoContainer">
-      <h1 className="page-title">{strings.en.info.title}</h1>
       <section className="infoSection">
-        <p className="infoParagraph">{strings.en.info.welcome}</p>
-        <p className="infoParagraph">{strings.en.info.mission}</p>
+        <p className="infoParagraph">
+          {strings.en.description.first_paragraph}
+        </p>
       </section>
     </div>
   );
