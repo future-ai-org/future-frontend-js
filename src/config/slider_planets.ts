9c7e3757158fdc28d6379ef53b5ad5e5efdc8028
von-steinkirch <b@bt3gl.xyz>
2025-05-23 16:56:23 +0200

    feat: add some nice changes to slider and info

--- src/config/slider_planets.ts
+++ src/config/slider_planets.ts
@@ -1,3 +1,3 @@
 export const LOS_ANGELES_LATITUDE = 34.0522;
 export const LOS_ANGELES_LONGITUDE = -118.2437;
-export const REFRESH_INTERVAL = 60000; // 1 minute in milliseconds
+export const REFRESH_INTERVAL = 1800000; // 30 minutes in milliseconds
