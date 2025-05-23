37b1747ad7e143b1d58218a0ceeea2c1d2b71f04
von-steinkirch <b@bt3gl.xyz>
2025-05-23 13:04:12 +0200

    feat: add info box

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,9 +1,22 @@
-import Info from "../../components/Info";
+"use client";
+
+import React from "react";
+import "../../styles/info.css";
+import strings from "../../i18n/info.json";
 
 export default function InfoPage() {
   return (
-    <main className="min-h-screen">
-      <Info />
+    <main>
+      <div className="info-container">
+        <h1 className="info-title">{strings.en.title}</h1>
+        <div className="info-box">
+          <div className="info-content">
+            <p>{strings.en.description.first_paragraph}</p>
+            <p>{strings.en.description.second_paragraph}</p>
+            <p>{strings.en.description.third_paragraph}</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
