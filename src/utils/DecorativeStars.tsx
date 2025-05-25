e03df2a0ca54ea0c0e8dba20a76d88c65e4bed94
von-steinkirch <b@bt3gl.xyz>
2025-05-25 23:20:55 +0200

    feat: change the size of the stars

--- src/utils/DecorativeStars.tsx
+++ src/utils/DecorativeStars.tsx
@@ -27,10 +27,10 @@ const DecorativeStars: React.FC = () => {
   const shuffledPositions = positions.sort(() => Math.random() - 0.5);
 
   // Take more stars
-  const stars = shuffledPositions.slice(0, 35).map((pos, index) => {
+  const stars = shuffledPositions.slice(0, 40).map((pos, index) => {
     const twinkleDelay = Math.random() * 2;
     const floatDelay = Math.random() * 7;
-    const size = Math.random() * 9 + 5; // Size range: 3-12px
+    const size = Math.random() * 15 + 10; // Size range: 10-25px
     const opacity = Math.random() * 0.5 + 0.2; // Varying opacity: 0.2-0.7
 
     return (
