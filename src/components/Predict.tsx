a9fcc62647d74ca027a2b13fb4d1b49619732a51
von-steinkirch <b@bt3gl.xyz>
2025-05-25 23:18:29 +0200

    feat: start adding the predict page

--- src/components/Predict.tsx
+++ src/components/Predict.tsx
@@ -7,9 +7,10 @@ import strings from "../i18n/predict.json";
 export default function Predict() {
   return (
     <div className="predict-container">
-      <div className="predict-content">
-        <p className="predict-description">{strings.en.description}</p>
+      <div className="predict-header">
+        <h1 className="astrology-title">{strings.en.title}</h1>
       </div>
+      <div className="predict-content"></div>
     </div>
   );
 }
