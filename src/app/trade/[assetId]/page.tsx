e42a9e188df9fd914802a8ce6f3e2220bec74741
lilit <b@bt3gl.xyz>
2025-05-28 00:30:39 +0200

    more title adj in the logia page

--- src/app/trade/[assetId]/page.tsx
+++ src/app/trade/[assetId]/page.tsx
@@ -4,7 +4,8 @@ import { TradeAsset } from "../../../components/TradeAsset";
 
 export default function TradePage({ params }: { params: { assetId: string } }) {
   return (
-    <main>
+    <main className="min-h-screen mt-8">
+      <h1 className="trade-title">Trading Chart</h1>
       <TradeAsset assetId={params.assetId} />
     </main>
   );
