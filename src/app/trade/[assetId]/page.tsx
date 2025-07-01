82000fd4040f354bcbfaa7b1e4219f00fae67857
von-steinkirch <b@bt3gl.xyz>
2025-06-30 20:50:22 -1000

    bug: update to next

--- src/app/trade/[assetId]/page.tsx
+++ src/app/trade/[assetId]/page.tsx
@@ -2,14 +2,16 @@
 
 import { TradeAsset } from "../../../components/TradeAsset";
 import { useTranslation } from "react-i18next";
+import { useParams } from "next/navigation";
 
-export default function TradePage({ params }: { params: { assetId: string } }) {
+export default function TradePage() {
+  const params = useParams();
   const { t } = useTranslation("tradeAsset");
 
   return (
     <main className="min-h-screen mt-8">
       <h1 className="trade-title">{t("titles.tradingChart")}</h1>
-      <TradeAsset assetId={params.assetId} />
+      <TradeAsset assetId={params.assetId as string} />
     </main>
   );
 }
