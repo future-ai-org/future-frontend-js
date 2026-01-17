4b545fcf247ed36bc4e0266b8d9ddd763baa7fd1
random-anon333 <contact@vonsteinkirch.com>
2026-01-17 19:03:55 +0800

    remove price bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { SliderPrice } from "./SliderPrice";
 import { SliderPlanets } from "./SliderPlanets";
 import styles from "../styles/footer.module.css";
 
@@ -9,7 +8,6 @@ export const Footer: React.FC = () => {
   return (
     <footer className={styles.footerContainer}>
       <div className="w-full">
-        <SliderPrice />
         <SliderPlanets />
       </div>
     </footer>
